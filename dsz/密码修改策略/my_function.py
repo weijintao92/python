ccf6f6ead247c535f55e392a88f813f4a83891fe
unknown <1109765190@qq.com>
2020-07-22 18:37:09 +0800

    密码修改

---
+++ dsz/密码修改策略/my_function.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class my_function:
+
+
+def func_1():
+    print('1')
+    # try:
+    #     # 获取计算机用户名
+    #     user = getpass.getuser()
+    #     my_resole =  subprocess.Popen(['net', 'User', user, 'weijintao92'])  # 在本地执行（类似于cmd命令）
+    #     print(my_resole)
+    # except expression as identifier:
+    #     print('失败！')
+
+    # my_xml = open(file_path, 'rb')
+    # my_xml = my_xml.read()
+    # my_soup = BeautifulSoup(my_xml, 'xml')
+    # # print(my_soup)
+    # print(my_soup.configuration.connectionStrings.add['connectionString'])
+    # print(my_soup)
+    # 将BeautifulSoup保存为文件
+    # with open(file_path, 'wb') as fd:
+    #     fd.write(my_soup.encode('utf-8'))
+    # my_xml.close()
+    # print(my_soup.configuration.connectionStrings.add)
+    # my_xml.flush()
--- dsz/密码修改策略/update_pwd.py
+++ dsz/密码修改策略/update_pwd.py
@@ -1,10 +1,54 @@
 import getpass
 import subprocess
+from bs4 import BeautifulSoup
+import tkinter as tk
+from tkinter import filedialog
+from my_function import my_function
 
-try:
-    # 获取计算机用户名
-    user = getpass.getuser()
-    my_resole =  subprocess.Popen(['net', 'User', user, 'weijintao92'])  # 在本地执行（类似于cmd命令）
-    print(my_resole)
-except expression as identifier:
-    print('失败！')
+# 第1步，实例化object，建立窗口window
+window = tk.Tk()
+# 第2步，给窗口的可视化起名字
+window.title('大数智')
+# 第3步，设定窗口的大小(长 * 宽)
+window.geometry('500x300')  # 这里的乘是小x
+# 窗体加载时运行
+# root.withdraw()
+path_api = ""
+path_web = ""
+
+# 选择文件的方法
+
+
+def checkFile_api():
+    global path_api
+    path_api = tk.filedialog.askopenfilename()
+
+
+def checkfile_web():
+    global path_web
+    path_web = tk.filedialog.askopenfilename()
+
+
+def my_strat():
+    my_function.func_1()
+
+
+# 选择数据源
+btn_api = tk.Button(window, text='选择API配置文件', font=(
+    'Arial', 12), command=checkFile_api)
+btn_api.pack()
+# 选择数据源
+btn_web = tk.Button(window, text='选择WEB配置文件', font=(
+    'Arial', 12), command=checkfile_web)
+btn_web.pack()
+
+# 选择数据源
+btn_start = tk.Button(window, text='开始', font=(
+    'Arial', 12), command=my_strat)
+btn_start.pack()
+
+
+# 第5步，主窗口循环
+window.mainloop()
+
+
